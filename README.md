6633504988af14df529b998ec392d8c6e11d2a47
rushikeshmore <rushikeshmore271@gmail.com>
2024-04-04 00:52:34 +0530

    Added style and init the libs in js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Things to know for testing 🛠️
+Please note that we've implemented the Slick JS slider with the 'mobileFirst' setting enabled, meaning it's initialized specifically for mobile devices. As a result, when testing our website on mobile breakpoints, you may need to refresh the page to view the slider properly.
 
+Additionally, we've integrated the fancyapps.com library to enhance the user experience. Clicking on any image will open it in full view, providing an enriched viewing experience. Thank you for your understanding as we strive to optimize the user experience across different devices.
 <br>
--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
 console.log('loading index.js file...')
+function initSlider() {
+    $('#main_content').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 920,
+                settings: "unslick"
+            }
+        ]
+    });
+}
+  
+initSlider();
+
+// fancybox init
+Fancybox.bind('[data-fancybox="gallery"]', {
+    dragToClose: false,
+    
+    Toolbar: {
+        display: {
+        left: ['close'],
+        middle: [],
+        right: [],
+        },
+    },
+    
+    Images: {
+        zoom: false,
+    },
+    
+    Thumbs: {
+        type: 'classic',
+    },
+    
+    Carousel: {
+        transition: false,
+        friction: 0,
+    },
+    
+    on: {
+        'Carousel.ready Carousel.change': (fancybox) => {
+        fancybox.container.style.setProperty(
+            '--bg-image',
+            `url("${fancybox.getSlide().thumbSrc}")`
+        );
+        },
+    },
+    });
+    
